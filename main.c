5fee48f23c4ee5b9a26f9afbbf528738e7e2a626
skyshiro <hodge.tannis@gmail.com>
2014-05-19 15:51:56 -0700

    Implemented ADC
    ADC is determining mic value, doing a continuous conversion of multiple
    channels. Will attempt repeated conversion of single channels.

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include <msp430.h> 
 
+#define MIC0 BIT0
 #define MIC1 BIT1
 #define MIC2 BIT2
 #define MIC3 BIT3
 #define MIC4 BIT4
-#define MIC5 BIT5
 
 #define MIC_CAL 20
-
-//Voltage reference ~3V
-#define VOLT_REF BIT0
 
 #define WIN_TIME 5
 
@@ -17,6 +14,17 @@
  * P1.1 through P1.5 are the microphone inputs MIC1 through MIC5
  *
  * P1.0 will be the reference voltage
+ *
+ * ADC value of about 1020 will be rail
+ *
+ * CONSEQ_3 will convert selected channels continously
+ *
+ * ADC10ON = 1
+ *
+ * set INCH_1 so ADC starts at channel 1 and goes to channel 0
+ *
+ * ENC = 1
+ * ADC10SC = 1
  */
 
 int virgin_flag;
@@ -26,24 +34,21 @@ int mic_check;										//BITN in mic_check is for MICN+1. Determines which MIC 
 int mic_num;										//determine which MIC is being used
 int mic_use;										//vector that determines mic being used
 
+int mic_adc[5];
+
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;						// Stop watchdog timer
 	BCSCTL1 = CALBC1_16MHZ;
 	DCOCTL = CALDCO_16MHZ;
 	_enable_interrupts();
 
-    P1DIR |= MIC1 + MIC2 + MIC3 + MIC4 + MIC5 + VOLT_REF;		//configure microphone & reference voltage inputs
-
     virgin_flag = 1;
     mic_check = 0x00;
 
-    CACTL2 |= P2CA0;											//select reference voltage
-    CACTL2 |= P2CA3 + P2CA2 + P2CA1;							//switch to CA7 pin that's grounded to prevent floating pin
-
-    CACTL1 |= CAON + CAF;												//turn on comparator
-    CACTL1 &= ~CAIFG;											//clear interrupt flag because it accidentally triggers
-    CACTL1 |= CAIE + CAIES;										//enable comparator interrupts
-
+    ADC10CTL1 |= INCH_2 + ADC10SSEL_2 + CONSEQ_3;
+    ADC10CTL0 |= ADC10IE + MSC + ADC10SHT_2;
+    ADC10CTL0 |= ADC10ON;								//turn on the ADC and interrupts for it
+    ADC10CTL0 |= ENC + ADC10SC;
     //sweep microphone input
 
     //for 5 mic setup
@@ -52,23 +57,10 @@ int main(void) {
     //for 2 mic setup
     while(mic_check != 0x3)
     {
-    	if(~mic_check & (MIC1 >> 1))
-    	{
-    		mic_use = 0;
-    		CACTL2 &= ~(P2CA2 + P2CA3);							//selects CA1 from CA7 to prevent floating pin
-    		//__delay_cycles(WIN_TIME);
-    		CACTL2 |= P2CA1 + P2CA2 + P2CA3;					//switch to CA7, grounded input
 
-    	}
-
-    	if(~mic_check & (MIC2 >> 1))
-		{
-    		mic_use = 1;
-    		CACTL2 &= ~(P2CA1 + P2CA3);								//selects CA2 from CA7 to prevent floating pin
-    		//__delay_cycles(WIN_TIME);
-    		CACTL2 |= P2CA1 + P2CA2 + P2CA3;						//switch to CA7, grounded input
-		}
     }
+
+    ADC10CTL0 &= ~ADC10ON;										//turn off ADC when done filling with values
 
     //mic_time should be around 4660 for 10cm
     while(1);
@@ -76,26 +68,30 @@ int main(void) {
 	return 0;
 }
 
-//ISR for comparator
-#pragma vector=COMPARATORA_VECTOR
-__interrupt void comp_isr (void)
+//ISR for ADC
+#pragma vector=ADC10_VECTOR
+__interrupt void adc_isr (void)
 {
-	mic_check |= 1 << mic_use;
+	//determine which mic was converted
 
-	if(virgin_flag)
+	mic_num = 0xF & (ADC10CTL1 >> 12);					//determine which microphone was converted
+
+	if(ADC10MEM > 1000)									//check is ADC value is close to rail
 	{
-		//turn on the timer
-		TACTL = TASSEL_2 + MC_2;                  // SMCLK, continous mode @ 16MHz
+		mic_check |= 1 << mic_num;
 
-		virgin_flag = 0;
-		mic_time[mic_num] = 0;
+		if(virgin_flag)
+		{
+			//turn on the timer
+			TACTL = TASSEL_2 + MC_2;                  // SMCLK, continous mode @ 16MHz
+
+			virgin_flag = 0;
+			mic_time[mic_num] = 0;
+		}
+		else
+		{
+			//mic_time[mic_use] = TAR-MIC_CAL;		//the value of the next microphone is the value of TAR
+			mic_time[mic_use] = TAR;
+		}
 	}
-	else
-	{
-		//mic_time[mic_use] = TAR-MIC_CAL;		//the value of the next microphone is the value of TAR
-		mic_time[mic_use] = TAR;
-	}
-
-	CACTL1 &= ~CAIFG;
-
 }
