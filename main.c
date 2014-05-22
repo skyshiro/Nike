85de90d660e2fdc70f682d2057959751ae2f06e1
skyshiro <hodge.tannis@gmail.com>
2014-05-22 09:46:57 -0700

    3 Microphone Array
    Need to make array length larger.

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <msp430.h> 
+#include <math.h>
+
 
 #define MIC0 BIT0
 #define MIC1 BIT1
@@ -7,11 +9,14 @@
 #define MIC4 BIT4
 
 #define MIC_CAL 20
-
+#define ADC_THRES 900
 #define WIN_TIME 5
 
+#define L10	.075
+#define L21	.075
+
 /*
- * P1.1 through P1.5 are the microphone inputs MIC1 through MIC5
+ * P1.1 through P1.5 are the microphone inputs MIC0 through MIC4
  *
  * P1.0 will be the reference voltage
  *
@@ -33,12 +38,15 @@ int convert_flag;									//determines if ADC has converted value
 int mic_time[5];									//holds value of microphone
 int mic_adc[5];
 int mic_check;										//BITN in mic_check is for MICN. Determines which MIC has a time value
-int mic_num;										//determine which MIC is being used
 int mic_use;										//vector that determines mic being used
 
 int mic_adc[5];
 
+float test, CD10, CD21,R_horz,B_horz;
+
 int main(void) {
+
+
     WDTCTL = WDTPW | WDTHOLD;						// Stop watchdog timer
 	BCSCTL1 = CALBC1_16MHZ;
 	DCOCTL = CALDCO_16MHZ;
@@ -58,7 +66,7 @@ int main(void) {
     //while(!(mic_check & 0x1F))								//when all bits in mic_check vector are set, exit while loop to continue
 
     //for 2 mic setup
-    while(mic_check != 0x3)
+    while(mic_check != 0x7)
     {
     	if(~mic_check & MIC0)
     	{
@@ -76,14 +84,32 @@ int main(void) {
 			convert_flag = 1;
 			ADC10CTL1 &= ~INCH_1;
 		}
+    	if(~mic_check & MIC2)
+		{
+			mic_use = 2;
+			ADC10CTL1 |= INCH_2;
+			ADC10CTL0 |= ADC10SC+ENC;;
+			while(convert_flag);
+			convert_flag = 1;
+			ADC10CTL1 &= ~INCH_2;
+		}
 
     }
 
+
     ADC10CTL0 &= ~ADC10ON;										//turn off ADC when done filling with values
 
-    //mic_time should be around 4660 for 10cm
+    CD10 = (mic_time[1]-mic_time[0])/4000.0*343/4000.0;
+    CD21 = (mic_time[2]-mic_time[1])/4000.0*343/4000.0;
+
+    R_horz = L10 * ( 1 - ( (CD10 / L10)*(CD10 / L10)) ) + L21 * ( 1 - ( (CD21 / L21 ) * (CD21 / L21) ));
+    R_horz = R_horz / ( 2 * ( ( CD21 / L21 ) - ( CD10 / L10 ) ) );
+    R_horz = R_horz * 100;	//convert to cm
+
+    B_horz = acos(( L10*L10 - 2*R_horz*CD21 - CD21*CD21) / ( 2*R_horz*L10 ))*180/3.14159;
+
     while(1);
-	
+
 	return 0;
 }
 
@@ -97,7 +123,7 @@ __interrupt void adc_isr (void)
 
 	mic_adc[mic_use] = ADC10MEM;
 
-	if(ADC10MEM > 1000)									//check is ADC value is close to rail
+	if(ADC10MEM > ADC_THRES)									//check is ADC value is close to rail
 	{
 		mic_check |= 1 << mic_use;
 
